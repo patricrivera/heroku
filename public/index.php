11b09e05c522810652d3bd61aeaf8c2b98f7ca56
Patric Rivera <patric.rivera@honcho.com>
2018-03-21 11:04:37 +0800

    Revert: Test Edit

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+print "<pre>"; //@fixme: <PATRIC> - Remove me 
+var_dump('asd');
+print "</pre>";
+die();
+
 // Delegate static file requests back to the PHP built-in webserver
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
