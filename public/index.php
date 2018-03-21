654f5c3bcc1aa49c68f9f51a4fa6eaf549abf0f1
Patric Rivera <patric.rivera@honcho.com>
2018-03-21 11:08:06 +0800

    Revert: adjust path

--- public/index.php
+++ public/index.php
@@ -2,25 +2,20 @@
 
 declare(strict_types=1);
 
-print "<pre>"; //@fixme: <PATRIC> - Remove me 
-var_dump('asd');
-print "</pre>";
-die();
-
 // Delegate static file requests back to the PHP built-in webserver
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
 
-chdir(dirname(__DIR__));
-require 'vendor/autoload.php';
+#chdir(dirname(__DIR__));
+require '../vendor/autoload.php';
 
 /**
  * Self-called anonymous function that creates its own scope and keep the global namespace clean.
  */
 (function () {
     /** @var \Psr\Container\ContainerInterface $container */
-    $container = require 'config/container.php';
+    $container = require '../config/container.php';
 
     /** @var \Zend\Expressive\Application $app */
     $app = $container->get(\Zend\Expressive\Application::class);
@@ -28,8 +23,8 @@ require 'vendor/autoload.php';
 
     // Execute programmatic/declarative middleware pipeline and routing
     // configuration statements
-    (require 'config/pipeline.php')($app, $factory, $container);
-    (require 'config/routes.php')($app, $factory, $container);
+    (require '../config/pipeline.php')($app, $factory, $container);
+    (require '../config/routes.php')($app, $factory, $container);
 
     $app->run();
 })();
